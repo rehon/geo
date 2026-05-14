a83b2d8c77e1c3e72902e9f8d6e04142fdfcae35
Sergey <test@mail.ru>
2026-05-14 12:53:40 +0500

    Разработанн класс для обработки данных по url

--- app/src/main/java/com/example/geoexam/MainActivity.java
+++ app/src/main/java/com/example/geoexam/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.geoexam;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,5 +36,36 @@ public class MainActivity extends AppCompatActivity {
         main_btn = findViewById(R.id.main_btn);
         user_history = findViewById(R.id.user_history);
         result = findViewById(R.id.result);
+    }
+
+    protected class GetDataUrl extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder buffer = new StringBuilder();
+                String line = "";
+
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line).append("\n");
+                }
+
+                return buffer.toString();
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
